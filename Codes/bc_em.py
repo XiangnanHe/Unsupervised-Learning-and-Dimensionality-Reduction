72583d446a3b875fd048db96454e287b71539268
Shawn <hexiangnan@gmail.com>
2018-07-11 06:17:12 -0700

    Add files via upload

---
+++ Codes/bc_em.py
@@ -0,0 +1,35 @@
+
+from sklearn import  datasets, metrics
+from clustertesters import ExpectationMaximizationTestCluster as emtc
+import pandas as pd
+
+def encode_target(df, target_column):
+    """Add column to df with integers for the target.
+
+    Args
+    ----
+    df -- pandas DataFrame.
+    target_column -- column to map to int, producing
+                     new Target column.
+
+    Returns
+    -------
+    df_mod -- modified DataFrame.
+    targets -- list of target names.
+    """
+    df_mod = df.copy()
+    targets = df_mod[target_column].unique()
+    map_to_int = {name: n for n, name in enumerate(targets)}
+    df_mod[target_column].replace(map_to_int, inplace=True)
+    return (df_mod, map_to_int)
+
+if __name__ == "__main__":
+    breast_cancer = pd.read_csv("wisconsin.csv")
+    dft, mapping = encode_target(breast_cancer, "class")
+
+    X = (dft.ix[:, :-1])
+    y = dft.ix[:, -1]
+
+    tester = emtc.ExpectationMaximizationTestCluster(X, y, clusters=range(1,30), plot=True, targetcluster=2, stats=True)
+    tester.run()
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+This report includes using both Weka and Scikit-Learn.
+1)Python sklearn were used in the clustering algorithms for K-means and EM, also for ANN.
+2)Java was used for DR analysis with J48 performance comparison. (The lower ranked components are added one per step for the evaluation)
+3)The original datasets are included in "Codes" folder and are named letter.arff(or letter.csv) and wisconsin.arff(or wisconsin.csv).
+4)Download and use Weka for dimensionality reduction with PCA, ICA, RP and IG algorithms. FastICA needs to be downloaded for ICA, here is the link: https://github.com/cgearhart/students-filters
+5)Save the data to arff and csv files for feeding to java code or python code to do dimensionality reduction analysis and clustering analysis. 
+6)The final plots are mostly done with Excel.
